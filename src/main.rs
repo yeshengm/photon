939d77c29b675f676492adb45615d04747df3407
Yesheng Ma <kimi.ysma@gmail.com>
2020-02-10 21:50:31 -0800

    rgb, position, and ray

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,131 @@
-#[derive(Clone)]
+#[derive(Clone, Copy)]
+struct Color {
+    r: f32,
+    g: f32,
+    b: f32,
+}
+
+impl Color {
+    fn new(r: f32, g: f32, b: f32) -> Color {
+        Color { r, g, b }
+    }
+}
+
+impl std::default::Default for Color {
+    fn default() -> Color {
+        Color {
+            r: 0.0,
+            g: 0.0,
+            b: 0.0,
+        }
+    }
+}
+
+impl std::ops::Add<Color> for Color {
+    type Output = Color;
+    fn add(self, rhs: Color) -> Color {
+        Color::new(self.r + rhs.r, self.g + rhs.g, self.b + rhs.b)
+    }
+}
+
+impl std::ops::Sub for Color {
+    type Output = Color;
+    fn sub(self, rhs: Color) -> Color {
+        Color::new(self.r - rhs.r, self.g - rhs.g, self.b - rhs.b)
+    }
+}
+
+impl std::ops::Neg for Color {
+    type Output = Color;
+    fn neg(self) -> Color {
+        Color::new(-self.r, -self.g, -self.b)
+    }
+}
+
+impl std::ops::Mul<f32> for Color {
+    type Output = Color;
+    fn mul(self, rhs: f32) -> Color {
+        Color::new(self.r * rhs, self.g * rhs, self.b * rhs)
+    }
+}
+
+impl std::ops::Div<f32> for Color {
+    type Output = Color;
+    fn div(self, rhs: f32) -> Color {
+        Color::new(self.r / rhs, self.g / rhs, self.b / rhs)
+    }
+}
+
+#[derive(Clone, Copy)]
 struct Vec3 {
-    e: [f32; 3],
+    x: f32,
+    y: f32,
+    z: f32,
 }
 
 impl Vec3 {
-    fn new(e0: f32, e1: f32, e2: f32) -> Vec3 {
-        Vec3 { e: [e0, e1, e2] }
-    }
-
-    fn x(&self) -> f32 {
-        self.e[0]
-    }
-
-    fn y(&self) -> f32 {
-        self.e[1]
-    }
-
-    fn z(&self) -> f32 {
-        self.e[2]
-    }
-
-    fn r(&self) -> f32 {
-        self.e[0]
-    }
-
-    fn g(&self) -> f32 {
-        self.e[1]
-    }
-
-    fn b(&self) -> f32 {
-        self.e[2]
+    fn new(x: f32, y: f32, z: f32) -> Vec3 {
+        Vec3 { x, y, z }
     }
 
     fn length(&self) -> f32 {
-        f32::sqrt(self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2])
+        f32::sqrt(self.x * self.x + self.y * self.y + self.z * self.z)
     }
 
     fn sum_of_squares(&self) -> f32 {
-        self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
+        self.x * self.x + self.y * self.y + self.z * self.z
     }
 
     fn to_unit_vector(&self) -> Vec3 {
-        let denom =
-            f32::sqrt(self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]);
+        let denom = f32::sqrt(self.x * self.x + self.y * self.y + self.z * self.z);
         Vec3 {
-            e: [self.e[0] / denom, self.e[1] / denom, self.e[2] / denom],
+            x: self.x / denom,
+            y: self.y / denom,
+            z: self.z / denom,
         }
     }
 
     fn dot(v1: Vec3, v2: Vec3) -> Vec3 {
-        Vec3::new(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2])
+        Vec3::new(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z)
     }
 
     fn cross(v1: Vec3, v2: Vec3) -> Vec3 {
         Vec3::new(
-            v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1],
-            v1.e[2] * v2.e[0] - v1.e[0] * v2.e[2],
-            v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0],
+            v1.y * v2.z - v1.z * v2.y,
+            v1.z * v2.x - v1.x * v2.z,
+            v1.x * v2.y - v1.y * v2.x,
         )
     }
 }
 
 impl std::default::Default for Vec3 {
     fn default() -> Vec3 {
-        Vec3 { e: [0.0, 0.0, 0.0] }
+        Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        }
     }
 }
 
 impl std::ops::Add<Vec3> for Vec3 {
     type Output = Vec3;
     fn add(self, rhs: Vec3) -> Vec3 {
-        Vec3::new(
-            self.e[0] + rhs.e[0],
-            self.e[1] + rhs.e[1],
-            self.e[2] + rhs.e[2],
-        )
+        Vec3::new(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
     }
 }
 
 impl std::ops::Sub for Vec3 {
     type Output = Vec3;
     fn sub(self, rhs: Vec3) -> Vec3 {
-        Vec3::new(
-            self.e[0] - rhs.e[0],
-            self.e[1] - rhs.e[1],
-            self.e[2] - rhs.e[2],
-        )
+        Vec3::new(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
     }
 }
 
 impl std::ops::Neg for Vec3 {
     type Output = Vec3;
     fn neg(self) -> Vec3 {
-        Vec3::new(-self.e[0], -self.e[1], -self.e[2])
+        Vec3::new(-self.x, -self.y, -self.z)
     }
 }
 
@@ -100,11 +133,7 @@ impl std::ops::Neg for Vec3 {
 impl std::ops::Mul<Vec3> for Vec3 {
     type Output = Vec3;
     fn mul(self, rhs: Vec3) -> Vec3 {
-        Vec3::new(
-            self.e[0] * rhs.e[0],
-            self.e[1] * rhs.e[1],
-            self.e[2] * rhs.e[2],
-        )
+        Vec3::new(self.x * rhs.x, self.y * rhs.y, self.z * rhs.z)
     }
 }
 
@@ -112,7 +141,7 @@ impl std::ops::Mul<Vec3> for Vec3 {
 impl std::ops::Mul<f32> for Vec3 {
     type Output = Vec3;
     fn mul(self, rhs: f32) -> Vec3 {
-        Vec3::new(self.e[0] * rhs, self.e[1] * rhs, self.e[2] * rhs)
+        Vec3::new(self.x * rhs, self.y * rhs, self.z * rhs)
     }
 }
 
@@ -120,11 +149,7 @@ impl std::ops::Mul<f32> for Vec3 {
 impl std::ops::Div<Vec3> for Vec3 {
     type Output = Vec3;
     fn div(self, rhs: Vec3) -> Vec3 {
-        Vec3::new(
-            self.e[0] * rhs.e[0],
-            self.e[1] * rhs.e[1],
-            self.e[2] * rhs.e[2],
-        )
+        Vec3::new(self.x / rhs.x, self.y / rhs.y, self.z / rhs.z)
     }
 }
 
@@ -132,83 +157,115 @@ impl std::ops::Div<Vec3> for Vec3 {
 impl std::ops::Div<f32> for Vec3 {
     type Output = Vec3;
     fn div(self, rhs: f32) -> Vec3 {
-        Vec3::new(self.e[0] * rhs, self.e[1] * rhs, self.e[2] * rhs)
-    }
-}
-
-impl std::ops::Index<usize> for Vec3 {
-    type Output = f32;
-    fn index(&self, index: usize) -> &f32 {
-        &self.e[index]
-    }
-}
-
-impl std::ops::IndexMut<usize> for Vec3 {
-    fn index_mut(&mut self, index: usize) -> &mut f32 {
-        &mut self.e[index]
+        Vec3::new(self.x / rhs, self.y / rhs, self.z / rhs)
     }
 }
 
 impl std::ops::AddAssign for Vec3 {
     fn add_assign(&mut self, rhs: Vec3) {
-        self.e[0] += rhs.e[0];
-        self.e[1] += rhs.e[1];
-        self.e[2] += rhs.e[2];
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
     }
 }
 
 impl std::ops::SubAssign for Vec3 {
     fn sub_assign(&mut self, rhs: Vec3) {
-        self.e[0] -= rhs.e[0];
-        self.e[1] -= rhs.e[1];
-        self.e[2] -= rhs.e[2];
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+        self.z -= rhs.z;
     }
 }
 
 impl std::ops::MulAssign<Vec3> for Vec3 {
     fn mul_assign(&mut self, rhs: Vec3) {
-        self.e[0] *= rhs.e[0];
-        self.e[1] *= rhs.e[1];
-        self.e[2] *= rhs.e[2];
+        self.x *= rhs.x;
+        self.y *= rhs.y;
+        self.z *= rhs.z;
     }
 }
 
 impl std::ops::MulAssign<f32> for Vec3 {
     fn mul_assign(&mut self, rhs: f32) {
-        self.e[0] *= rhs;
-        self.e[1] *= rhs;
-        self.e[2] *= rhs;
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
     }
 }
 
 impl std::ops::DivAssign<Vec3> for Vec3 {
     fn div_assign(&mut self, rhs: Vec3) {
-        self.e[0] /= rhs.e[0];
-        self.e[1] /= rhs.e[1];
-        self.e[2] /= rhs.e[2];
+        self.x /= rhs.x;
+        self.y /= rhs.y;
+        self.z /= rhs.z;
     }
 }
 
 impl std::ops::DivAssign<f32> for Vec3 {
     fn div_assign(&mut self, rhs: f32) {
-        self.e[0] /= rhs;
-        self.e[1] /= rhs;
-        self.e[2] /= rhs;
+        self.x /= rhs;
+        self.y /= rhs;
+        self.z /= rhs;
     }
+}
+
+// a ray is defined by: p(t) = A + B * t
+struct Ray {
+    A: Vec3,
+    B: Vec3,
+}
+
+impl Ray {
+    fn new(A: Vec3, B: Vec3) -> Ray {
+        Ray { A, B }
+    }
+
+    fn origin(&self) -> Vec3 {
+        self.A
+    }
+
+    fn direction(&self) -> Vec3 {
+        self.B
+    }
+
+    fn point_at(&self, t: f32) -> Vec3 {
+        self.A + self.B * t
+    }
+}
+
+impl std::default::Default for Ray {
+    fn default() -> Ray {
+        Ray {
+            A: Vec3::default(),
+            B: Vec3::default(),
+        }
+    }
+}
+
+fn color(r: &Ray) -> Color {
+    let unit_dir = r.direction().to_unit_vector();
+    let t = 0.5 * (unit_dir.y + 1.0);
+    Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
 }
 
 fn main() {
     let nx = 200;
     let ny = 100;
     print!("P3\n{} {}\n255\n", nx, ny);
+    let lower_left_corner = Vec3::new(-2.0, -1.0, -1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
 
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let vec = Vec3::new(i as f32 / nx as f32, j as f32 / ny as f32, 0.2);
-            let vec = vec * 255.99;
-            let ir = vec.r() as i32;
-            let ig = vec.g() as i32;
-            let ib = vec.b() as i32;
+            let u = i as f32 / nx as f32;
+            let v = j as f32 / ny as f32;
+            let ray = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v);
+            let c = color(&ray) * 255.99;
+            let ir = c.r as i32;
+            let ig = c.g as i32;
+            let ib = c.b as i32;
             println!("{} {} {}", ir, ig, ib);
         }
     }
